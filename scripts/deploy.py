b4731fa804704a224b58b69ffb2cbcfae094dde5
ArnaudTheBuilder <40626382+ArnoTower@users.noreply.github.com>
2022-06-06 01:58:03 -0400

    first

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -5,9 +5,11 @@ from scripts.helpful_scripts import get_account
 def deploy_fund_me():
     account = get_account()
     fund_me = FundMe.deploy({"from": account})
+
     # this deploy will make a state change to the blockchain we always need to do from account
+
     print(f"Contract deployed to {fund_me.address}")
 
 
 def main():
-    deploy_fund_me
+    deploy_fund_me()
