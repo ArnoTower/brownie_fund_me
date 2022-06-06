c3972d9872fc1febe8acf38dec0621c498ab6c82
ArnaudTheBuilder <40626382+ArnoTower@users.noreply.github.com>
2022-06-06 01:25:33 -0400

    first commit

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,7 +1,13 @@
 from brownie import FundMe
+from scripts.helpful_scripts import get_account
+
 
 def deploy_fund_me():
     account = get_account()
+    fund_me = FundMe.deploy({"from": account})
+    # this deploy will make a state change to the blockchain we always need to do from account
+    print(f"Contract deployed to {fund_me.address}")
+
 
 def main():
     deploy_fund_me
